ab6b05d4485590ffac14de0e03ed1899be97b784
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-05-18 17:26:44 +0300

    components part 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,51 +2,77 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    debugger
+    console.log("App rendering");
 
     return (
-        <div className="App">
-            Hello, samurai! Let's go!
+        <div>
+            <AppTitle />
             <Rating />
             <Accordion />
+            <Rating />
         </div>
+    );
+}
+
+function AppTitle() {
+
+    return(
+        <>This is App component</>
     );
 }
 
 function Rating() {
-    debugger
+    console.log("Rating rendering");
 
     return(
-        <div>
+        <>
             <Star />
             <Star />
             <Star />
             <Star />
             <Star />
-        </div>
+        </>
     );
 }
 
 function Star() {
-
+    console.log("Star rendering");
     return(
         <div>star</div>
     );
 }
 
 function Accordion() {
-    debugger
+    console.log("Accordion rendering");
 
     return (
-        <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
+        <>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </>
+
     );
 }
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendering");
+
+    return(
+        <h3>Menu</h3>
+    );
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering");
+
+    return(
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
+}
+
 
 export default App;
