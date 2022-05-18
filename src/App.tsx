78e500e715d58abb14b80c6a1d7839f6cbab11c5
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-05-18 16:37:46 +0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,51 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    debugger
+
+    return (
+        <div className="App">
+            Hello, samurai! Let's go!
+            <Rating />
+            <Accordion />
+        </div>
+    );
+}
+
+function Rating() {
+    debugger
+
+    return(
+        <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </div>
+    );
+}
+
+function Star() {
+
+    return(
+        <div>star</div>
+    );
+}
+
+function Accordion() {
+    debugger
+
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
