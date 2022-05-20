5bcf08c21791dcfb209b6bba5b0649b23dff65e8
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-05-20 18:13:44 +0300

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,42 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
     console.log("App rendering");
 
     return (
         <div>
-            <AppTitle />
-            <Rating />
-            <Accordion />
-            <Rating />
+            {/*<PageTitle title={"This is App component"}/>
+            <PageTitle title={"Users"}/>
+            Article 1
+            <Rating value={3}/>*/}
+            <Accordion titleValue={"Menu"} collapsed={true}/>
+            <Accordion titleValue={"Users"} collapsed={false}/>
+            {/*Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>*/}
         </div>
     );
 }
 
-function AppTitle() {
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
 
     return(
-        <>This is App component</>
+        <h1>{props.title}</h1>
     );
 }
 
-function Rating() {
-    console.log("Rating rendering");
 
-    return(
-        <>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-        </>
-    );
-}
-
-function Star() {
-    console.log("Star rendering");
-    return(
-        <div>star</div>
-    );
-}
-
-function Accordion() {
-    console.log("Accordion rendering");
-
-    return (
-        <>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </>
-
-    );
-}
-
-function AccordionTitle() {
-    console.log("AccordionTitle rendering");
-
-    return(
-        <h3>Menu</h3>
-    );
-}
-
-function AccordionBody() {
-    console.log("AccordionBody rendering");
-
-    return(
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    );
-}
 
 
 export default App;
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+type AccordionPropsType = {
+    titleValue: string,
+    collapsed: boolean
+}
+
+function Accordion(props: AccordionPropsType) {
+    console.log("Accordion rendering");
+    if(props.collapsed){
+        return <AccordionTitle titleValue={props.titleValue}/>
+    } else{
+        return (
+            <>
+                <AccordionTitle titleValue={props.titleValue}/>
+                <AccordionBody/>
+            </>
+        );
+    }
+}
+
+type AccordionTitlePropsType = {
+    titleValue: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log("AccordionTitle rendering");
+    return(
+        <h3>---{props.titleValue}---</h3>
+    );
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering");
+
+    return(
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
+}
+
+export default Accordion;
