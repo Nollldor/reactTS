f7fd6903d0c1803b19054fcf8b3de67633af5efd
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-09-29 15:08:01 +0300

    simpleClock fix

--- src/components/SimpleClock/SimpleClock.tsx
+++ src/components/SimpleClock/SimpleClock.tsx
@@ -1,10 +1,5 @@
 import React, {useEffect, useState} from "react";
 
-export default {
-    title: 'SimpleClock demo'
-}
-
-
 export const SimpleClock = () => {
     const [date, setDate] = useState(new Date())
 
---
+++ src/customStories/SimpleClock.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {SimpleClock} from "../components/SimpleClock/SimpleClock";
+
+export default {
+    title: 'SimpleClock demo'
+}
+
+
+export const SimpleClockStories = () => {
+    return <SimpleClock/>
+}
